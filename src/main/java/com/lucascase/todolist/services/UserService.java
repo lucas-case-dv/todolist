c1057d920cd458ee49541f380c236fb52f7944f3
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 16:52:29 -0300

    Creation of UserService

--- src/main/java/com/lucascase/todolist/services/UserService.java
+++ src/main/java/com/lucascase/todolist/services/UserService.java
@@ -1,4 +1,45 @@
 package com.lucascase.todolist.services;
 
+import com.lucascase.todolist.models.User;
+import com.lucascase.todolist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "User not found."));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        return this.userRepository.save(obj);
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = this.findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    @Transactional
+    public void delete(User obj) {
+        try {
+            this.userRepository.deleteById(obj.getId());
+        } catch (Exception e) {
+            throw new RuntimeException("Could not delete user.");
+        }
+    }
 }
