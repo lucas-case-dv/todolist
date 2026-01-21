b1bdcdbf5c6fb91f1caaef74ae7ba3c1066dbccb
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 15:12:09 -0300

    Creation of TaskController

---
+++ src/main/java/com/lucascase/todolist/controllers/TaskController.java
@@ -0,0 +1,51 @@
+package com.lucascase.todolist.controllers;
+
+import com.lucascase.todolist.models.Task;
+import com.lucascase.todolist.services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/task")
+@Validated
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        Task obj = this.taskService.findById(id);
+        return ResponseEntity.ok(obj); //Sends 200 with body
+    }
+
+    @PostMapping
+    @Validated
+    public ResponseEntity<Void> create(@Valid @RequestBody Task obj) {
+        this.taskService.create(obj);
+        URI uri =  ServletUriComponentsBuilder.fromCurrentRequest() //Creates location for new task
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build(); //Sends 201 with location
+    }
+
+    @PutMapping
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody Task obj, @PathVariable Long id) {
+        obj.setId(id); //Sets correct id
+        this.taskService.update(obj);
+        return ResponseEntity.noContent().build(); //Sends 204 without content
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
