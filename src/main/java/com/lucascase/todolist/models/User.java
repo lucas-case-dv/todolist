97b15a1f7085b44808798293089265eabefc33e9
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 16:38:02 -0300

    Creation of User model

---
+++ src/main/java/com/lucascase/todolist/models/User.java
@@ -0,0 +1,74 @@
+package com.lucascase.todolist.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+
+    public static final String TABLE_NAME = "users";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 128)
+    @Column(name = "username", nullable = false, length = 128)
+    private String username;
+
+    @NotBlank
+    @Size(min = 4, max = 128)
+    @Column(name = "password", nullable = false, length = 128)
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+}
