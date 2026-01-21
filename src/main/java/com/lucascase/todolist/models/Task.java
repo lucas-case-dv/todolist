e1906e35e71fb61ad104812f807eb2ad79edd306
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 13:47:19 -0300

    Creation of Task model

---
+++ src/main/java/com/lucascase/todolist/models/Task.java
@@ -0,0 +1,61 @@
+package com.lucascase.todolist.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+//Task modelling for database
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+    public static final String TABLE_NAME = "tasks";
+
+    //Id for task
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
