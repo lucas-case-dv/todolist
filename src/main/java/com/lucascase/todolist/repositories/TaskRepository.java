c6c132bc1ef1577a189cdcdd89a2fd0e736b8774
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 13:56:14 -0300

    Creation of TaskRepository

---
+++ src/main/java/com/lucascase/todolist/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.lucascase.todolist.repositories;
+
+import com.lucascase.todolist.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
