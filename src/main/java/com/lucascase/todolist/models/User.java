ce37d57446fb3b38d6b8694d93728f71b7a71de7
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-01-21 19:34:26 -0300

    Refactor User model

--- src/main/java/com/lucascase/todolist/models/User.java
+++ src/main/java/com/lucascase/todolist/models/User.java
@@ -1,14 +1,21 @@
 package com.lucascase.todolist.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
+
+    public interface CreateUser {}
+    public interface UpdateUser {}
 
     public static final String TABLE_NAME = "users";
 
@@ -17,15 +24,19 @@ public class User {
     @Column(name = "id", unique = true)
     private Long id;
 
-    @NotBlank
-    @Size(min = 2, max = 128)
+    @NotBlank(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 128)
     @Column(name = "username", nullable = false, length = 128)
     private String username;
 
-    @NotBlank
-    @Size(min = 4, max = 128)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 4, max = 128)
     @Column(name = "password", nullable = false, length = 128)
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<>();
 
     public User() {
     }
@@ -58,6 +69,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @JsonIgnore
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
