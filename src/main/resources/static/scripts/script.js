9138d0e9cf62312ffdfcba9605fb2a05aeaad61d
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-02-03 13:27:28 -0300

    Fix delete task button

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -112,7 +112,7 @@ document.getElementById("task-list").addEventListener("click", function (e) {
 });
 
 function deleteTask(id) {
-    fetch("/task/${id}", {
+    fetch(`/task/${id}`, {
         method: "DELETE",
         headers: {
             Authorization: localStorage.getItem("token")
